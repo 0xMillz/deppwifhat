e6a3b5703405d53ec96856eda7c91654fb21890f
reubenstein <162486040+Reubenstein@users.noreply.github.com>
2024-05-27 23:27:51 -0500

    initial commit

---
+++ quotes.js
@@ -0,0 +1,19 @@
+const johnnyDeppQuotes = [
+    "The only creatures that are evolved enough to convey pure love are dogs and infants.",
+    "I think the thing to do is enjoy the ride while you're on it.",
+    "I think everybody's nuts.",
+    "I'm an old-fashioned guy... I want to be an old hat",
+    "I think the thing to do is enjoy the ride while you're on it.",
+    "People say I make strange choices, but they're not strange for me. My sickness is that I'm fascinated by human behavior, by what's underneath the surface, by the worlds inside people."
+    "I don't pretend to be captain weird. I just do what I do.",
+    "Life's pretty good, and why wouldn't it be? I'm a pirate, after all.",
+    "I am doing things that are true to me. The only thing I have a problem with is being labeled.",
+    "My favorite color is black. And I like black because it is a modest and arrogant color. Perhaps that's why I like it.",
+    "The only creatures that are evolved enough to convey pure love are dogs and infants.",
+    "Puberty was very vague. I literally locked myself in a room and played guitar.",
+    "As a teenager I was so insecure. I was the type of guy that never fitted in because he never dared to choose. I was convinced I had absolutely no talent at all. For nothing. And that thought took away all my ambition too.",
+    "I am doing things that are true to me. The only thing I have a problem with is being labeled.",
+    "I'm an old-fashioned guy... I want to be an old man with a beer belly sitting on a porch, looking at a lake or something.",
+    "Tomorrow it'll all be over, then I'll have to go back to selling pens again.
+
+]
